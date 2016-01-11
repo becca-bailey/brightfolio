4d709b1eb9db04a3bd459bb7fe30bfb0cead7071
Lauren Jin <lauren.jin@gmail.com>
2016-01-11 14:32:32 -0600

    Put back image content type

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20160110184226) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.string   "image_file_name"
-    # t.string   "image_content_type"
+    t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "document_file_name"

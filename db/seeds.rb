957dfd27f66494591c06ab6510410e79bc6fd2ad
deborahanneharris <harrisdeborahanne@gmail.com>
2016-01-13 16:31:58 -0600

    Add target blank functionality to element links

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-# require 'faker'
+require 'faker'
 
-# becca = User.create!(first_name: "Becca", last_name: "Nelson", email: "beccanelson88@gmail.com", password: "password")
-
-# 10.times do
-#   folio = becca.folios.create!(title: Faker::Hacker.adjective)
-#   20.times do
-#     element = folio.elements.create!(title: Faker::Hipster.sentence, element_link: Faker::Internet.url, description: Faker::Hipster.paragraph)
-#   end
-# end
+becca = User.create!(first_name: "Becca", last_name: "Nelson", email: "beccanelson88@gmail.com", password: "password")
+becca = User.find(1)
+10.times do
+  folio = becca.folios.create!(title: Faker::Hacker.adjective)
+  20.times do
+    element = folio.elements.create!(title: Faker::Hipster.sentence, element_link: Faker::Internet.url, description: Faker::Hipster.paragraph)
+  end
+end
